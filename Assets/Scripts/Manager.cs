f574cede0b237a5ac79119a399412b87d55eee42
unknown <cjernychcka@gmail.com>
2022-02-15 17:47:01 +0200

    Add UI components, implement ability to start game on firat click

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    GameObject[] startGameComponents;
+    GameObject[] gameOverUIComponents;
+    [SerializeField] GameObject score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGameComponents = GameObject.FindGameObjectsWithTag("start game");
+       gameOverUIComponents = GameObject.FindGameObjectsWithTag("game over");
+    DisableUIComponents (gameOverUIComponents);
+        EnableUIComponents(startGameComponents);
+        DisableScore(score);
+    }
+
+    public void GameOver()
+    {
+        EnableUIComponents(gameOverUIComponents);
+    }
+    
+    private void EnableScore(GameObject score)
+    {
+        score.SetActive(true);
+    }
+
+    private void DisableScore(GameObject score)
+    {
+        score.SetActive(false);
+    }
+
+    private void EnableUIComponents(GameObject[] startGameComponents)
+    {
+        foreach(GameObject obj in startGameComponents)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    private void DisableUIComponents(GameObject[] gameOverUIComponents)
+    {
+        foreach (GameObject obj in gameOverUIComponents)
+        {
+            obj.SetActive(false);
+        }
+
+        // Update is called once per frame
+       
+}
+}
---
+++ Assets/Scripts/Pipespawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipespawner : MonoBehaviour
+{
+    
+    [SerializeField] float timeToSpawn = 1f;
+    [SerializeField] float bordertop = 0.8f;
+    [SerializeField] float borderbottom =-0.2f;
+    [SerializeField] GameObject pipes;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+    public void StartSpawning()
+    {
+        StartCoroutine(spawnpipes());
+    }
+    IEnumerator spawnpipes()
+    {
+        while (true)
+        {
+        GameObject newPipe = Instantiate(pipes);
+        Vector3 randomFactor = new Vector3(0,Random.Range(borderbottom,bordertop),0);
+        newPipe.transform.position =transform.position+ randomFactor;
+        Destroy(newPipe, 6f);
+        yield return new WaitForSeconds(timeToSpawn);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
